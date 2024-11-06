a7bd9044e7e92fa9fe26970da6a85d8043da6ab5
mosesbejon <idontwantnumbersinmyname123@gmail.com>
2024-11-06 05:07:56 +0000

    Attempt at cursor feedback

--- dragLogic.js
+++ dragLogic.js
@@ -1,38 +1,38 @@
 // anything draggable uses functions from this file
 
-export function addDragLogicTo(element,drag,endDrag){
+export function addDragLogicTo(element,drag,endDrag,cursorHover = "grab",cursor = "grabbing"){
+    element.style.cursor = cursorHover
     element.onpointerdown = (pointerEvent) => {
-
-        // prevents the document attempting to drag the div/highlight text/other default behaviour
-        // while user is dragging edge
-        pointerEvent.preventDefault()
-        drag(pointerEvent)
-
-        document.addEventListener("pointermove",drag)
-
-        document.addEventListener("pointerup", (pointerEvent) => {
-            endDrag(pointerEvent)
-            document.removeEventListener("pointermove",drag)
-        },{
-            once: true
-        })
+        beginDragging(drag,endDrag,pointerEvent,cursor)
     }
 }
 
-export function addCreateLogicTo(element,create){
+export function addCreateLogicTo(element,create,cursorHover = "grab",cursor = "grabbing"){
+    element.style.cursor = cursorHover
+    console.log(cursorHover)
     element.onpointerdown = (pointerEvent) => {
-        pointerEvent.preventDefault()
-
         const newElement = create(pointerEvent)
-        newElement.drag(pointerEvent)
 
-        document.addEventListener("pointermove",newElement.drag)
-
-        document.addEventListener("pointerup",(pointerEvent) => {
-            newElement.endDrag(pointerEvent)
-            document.removeEventListener("pointermove",newElement.drag)
-        },{
-            once: true
-        })
+        beginDragging(newElement.drag,newElement.endDrag,pointerEvent,cursor)
     }
+}
+
+function beginDragging(drag,endDrag,pointerEvent,cursor){
+
+    // prevents the document attempting to drag the div/highlight text/other default behaviour
+    // while user is dragging edge
+    pointerEvent.preventDefault()
+
+    document.body.style.cursor = cursor
+
+    drag(pointerEvent)
+
+    document.addEventListener("pointermove",drag)
+    document.addEventListener("pointerup",(pointerEvent) => {
+        endDrag(pointerEvent)
+        document.body.style.cursor = "auto"
+        document.removeEventListener("pointermove",drag)
+    },{
+        once: true
+    })
 }
