99d460676708e56642b4c2466f3638d837683fac
mosesbejon <idontwantnumbersinmyname123@gmail.com>
2024-12-10 17:32:06 +0000

    Fixed issue with small ellipses

--- presentation/views/createModes/ellipseMode.js
+++ presentation/views/createModes/ellipseMode.js
@@ -9,9 +9,6 @@ export class ellipseMode{
     constructor(createCanvas) {
         this.createCanvas = createCanvas
 
-        this.currentShape = document.createElementNS("http://www.w3.org/2000/svg","g")
-        this.createCanvas.canvas.appendChild(this.currentShape)
-
         addDragLogicTo(this.createCanvas.canvas,
             this.continueEllipse.bind(this),
             this.endEllipse.bind(this),
@@ -30,13 +27,16 @@ export class ellipseMode{
     }
 
     beginEllipse(pointerEvent){
+        this.currentShape = document.createElementNS("http://www.w3.org/2000/svg","g")
+        this.createCanvas.canvas.appendChild(this.currentShape)
+
         this.initialPosition = this.createCanvas.toCanvasCoordinates(pointerEvent.clientX,pointerEvent.clientY)
 
         this.ellipse = document.createElementNS("http://www.w3.org/2000/svg","ellipse")
 
         if (this.createCanvas.fillColourToggled) {
             this.colour = this.createCanvas.fillColour.value
-            this.ellipse.style.color = this.colour
+            this.ellipse.style.fill = this.colour
         } else {
             this.colour = "transparent"
         }
@@ -61,10 +61,29 @@ export class ellipseMode{
         const height = Math.abs(this.initialPosition[1]-currentPosition[1])
         const width = Math.abs(this.initialPosition[0]-currentPosition[0])
 
+        /* check for edge case where there is not enough space in the user's ellipse for the outline */
+        if (this.thickness > height || this.thickness > width){
+
+            /* if there isn't the outline is removed and the entire ellipse becomes the outline */
+
+            this.ellipse.style.stroke = "transparent"
+            this.ellipse.style.fill = this.outlineColour
+
+            this.ellipse.setAttribute("rx",width/2 + this.thickness/2)
+            this.ellipse.setAttribute("ry",height/2 + this.thickness/2)
+        } else {
+
+            /* otherwise proceed as usual */
+
+            this.ellipse.style.stroke = this.outlineColour
+            this.ellipse.style.fill = this.colour
+
+            this.ellipse.setAttribute("rx",width/2)
+            this.ellipse.setAttribute("ry",height/2)
+        }
+
         this.ellipse.setAttribute("cx",centre[0])
         this.ellipse.setAttribute("cy",centre[1])
-        this.ellipse.setAttribute("rx",width/2)
-        this.ellipse.setAttribute("ry",height/2)
 
         return [width,height,centre]
     }
@@ -81,7 +100,7 @@ export class ellipseMode{
             0,
             this.thickness))
 
-        this.ellipse.remove()
+        this.currentShape.remove()
     }
 
 }
