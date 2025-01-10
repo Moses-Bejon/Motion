c52ef79174b1a1f73ff49e4407f5978e096d0703
mosesbejon <idontwantnumbersinmyname123@gmail.com>
2025-01-10 15:05:00 +0000

    Updated the Attributions

--- presentation/views/createModes/drawMode.js
+++ presentation/views/createModes/drawMode.js
@@ -26,9 +26,7 @@ export class drawMode extends manyPointsMode{
 
     endDrawing(pointerEvent){
 
-        console.log(this.pointArray.length)
         this.pointArray = decimateLine(this.pointArray,1)
-        console.log(this.pointArray.length)
 
         /* If the last and first points drawn on the shape are close enough together or there is no fill*/
         if (distanceBetween2dPoints(
