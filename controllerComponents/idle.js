821d8901f26873805d8d4c16aa105f0087ca1afb
mosesbejon <idontwantnumbersinmyname123@gmail.com>
2025-08-30 02:10:00 +0100

    relocated playing logic from idle to playing state

--- controllerComponents/idle.js
+++ controllerComponents/idle.js
@@ -1,8 +1,6 @@
 import {ReceivingActionState} from "./receivingAction.js";
 import {PlayingState} from "./playing.js";
 import {ExecutingScriptState} from "./executingScript.js";
-import {controller} from "../controller.js";
-import {animationEndTimeSeconds} from "../globalValues.js";
 
 export class IdleState {
     constructor() {
@@ -25,28 +23,6 @@ export class IdleState {
     }
 
     play(){
-
-        const animation = {"previousTime":performance.now()}
-
-        function nextFrame(animation){
-            const currentTime = performance.now()
-            const deltaTime = currentTime - animation.previousTime
-
-            let time = controller.clock()+deltaTime/1000
-
-            if (time > animationEndTimeSeconds){
-                time = 0
-            }
-
-            controller.currentScene.executeSteps([["goToTime",[time]]])
-
-            animation.previousTime = currentTime
-
-            animation.nextAnimationFrame = requestAnimationFrame(() => {nextFrame(animation)})
-        }
-
-        nextFrame(animation)
-
-        return new PlayingState(animation)
+        return new PlayingState()
     }
 }
--- controllerComponents/playing.js
+++ controllerComponents/playing.js
@@ -1,29 +1,52 @@
 import {IdleState} from "./idle.js";
+import {controller} from "../controller.js";
+import {animationEndTimeSeconds} from "../globalValues.js";
 
 export class PlayingState {
-    constructor(animation) {
-        this.animation = animation
+    constructor() {
+        this.previousTime = performance.now()
+        this.nextFrame()
+    }
+
+    nextFrame() {
+        const currentTime = performance.now()
+        const deltaTime = currentTime - this.previousTime
+
+        let time = controller.clock()+deltaTime/1000
+
+        if (time > animationEndTimeSeconds){
+            time = 0
+        }
+
+        controller.currentScene.executeSteps([["goToTime",[time]]])
+
+        this.previousTime = currentTime
+
+        this.nextAnimationFrame = requestAnimationFrame(this.nextFrame.bind(this))
     }
 
     beginAction(){
+        cancelAnimationFrame(this.nextAnimationFrame)
         throw new Error("can't begin an action while playing an animation")
     }
 
     takeStep(){
+        cancelAnimationFrame(this.nextAnimationFrame)
         throw new Error("can't take a step while playing an animation")
     }
 
     endAction(){
+        cancelAnimationFrame(this.nextAnimationFrame)
         throw new Error("can't end an action while playing an animation")
     }
 
     executeScript(script){
+        cancelAnimationFrame(this.nextAnimationFrame)
         throw new Error("can't execute a script while playing an animation")
     }
 
     play(){
-        cancelAnimationFrame(this.animation.nextAnimationFrame)
-
+        cancelAnimationFrame(this.nextAnimationFrame)
         return new IdleState()
     }
 }
